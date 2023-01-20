e34ac5fddfffb6f08933bdd8ccb06e2f5d7298b3
jos-the-top-dev <81918288+spectr-e@users.noreply.github.com>
2023-01-20 10:52:12 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,14 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import MoviesPage from "./MoviesPage";
+import React, { useState } from "react"
+import { Route, Switch } from "react-router-dom"
+import NavBar from "./NavBar"
+import MoviesPage from "./MoviesPage"
 
 function App() {
   const [movies, setMovies] = useState({
     1: { id: 1, title: "A River Runs Through It" },
     2: { id: 2, title: "Se7en" },
     3: { id: 3, title: "Inception" },
-  });
+  })
 
   return (
     <div>
@@ -22,7 +22,7 @@ function App() {
         </Route>
       </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
